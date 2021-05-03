6486e4068b0c96f81c5da058756ab56b0d487df2
Esmeray Tasgit <estasgit@gmail.com>
2021-05-03 16:31:54 +0200

    props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
-    return (
-        <div className="expense-item">
-        <div>{expenseDate}</div>
-        <div>
-           <h2 className="expense-item__description">{expenseTitle}</h2>
-           <div className="expense-item__price">${expenseAmount}</div>
-        </div>
-        </div>
-    )
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
 
 
 
