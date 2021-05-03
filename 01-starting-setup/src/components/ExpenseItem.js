bbd3456b46c0fb3616fb84d6d05f24ac92d240e8
Esmeray Tasgit <estasgit@gmail.com>
2021-05-03 16:01:03 +0200

    ExpenseItem data

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-        <div>March 28th 2021</div>
+        <div>{expenseDate}</div>
         <div>
-           <h2 className="expense-item__description">Car Insurance</h2>
-           <div className="expense-item__price">$294.67</div>
+           <h2 className="expense-item__description">{expenseTitle}</h2>
+           <div className="expense-item__price">${expenseAmount}</div>
         </div>
         </div>
     )
